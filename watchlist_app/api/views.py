7c2bf125d10571b79d70caa44dad5d212ef9a91f
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-17 20:01:36 -0300

    Add Movie view and serializer

---
+++ watchlist_app/api/views.py
@@ -0,0 +1,17 @@
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    print(movies)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def movie_detail(request, pk):
+    movie = Movie.objects.get(id=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)

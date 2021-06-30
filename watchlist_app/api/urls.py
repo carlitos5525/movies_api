acbeed850c265a9b0894c6f2f097eb1b3b52037e
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-30 20:14:17 -0300

    Add custom rating calculation

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('stream/', views.StreamPlatformListAV.as_view(), name='stream-list'),
     path('stream/<int:pk>/', views.StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
     
-    path('stream/<int:pk>/review', views.ReviewList.as_view(), name='review-list'),
-    path('stream/<int:pk>/review-create', views.ReviewCreate.as_view(), name='review-create'),
-    path('stream/review/<int:pk>', views.ReviewDetail.as_view(), name='review-detail'),
+    path('<int:pk>/reviews', views.ReviewList.as_view(), name='review-list'),
+    path('<int:pk>/review-create', views.ReviewCreate.as_view(), name='review-create'),
+    path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-detail'),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -196,8 +196,14 @@ class ReviewCreate(generics.CreateAPIView):
         review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie!")
+        if watchlist.number_ratings == 0:
+            watchlist.avg_rating = serializer.validated_data['rating']
         else:
-            serializer.save(watchlist=watchlist, review_user=review_user)
+            watchlist.avg_rating = ((watchlist.avg_rating * watchlist.number_ratings) + serializer.validated_data['rating'])/(watchlist.number_ratings + 1)
+        watchlist.number_ratings = watchlist.number_ratings + 1
+        watchlist.save()
+        
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):

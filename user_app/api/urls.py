0225f6760fa52b7dfe584ddee8a154a48855762e
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-07-03 18:52:01 -0300

    Add registration and login

---
+++ user_app/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
+from user_app.api import views
+
+urlpatterns = [
+    path('login/', obtain_auth_token, name='login'),
+    path('register/', views.registration_view, name='register'),
+]

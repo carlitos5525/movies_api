4ed13e34c84ded1f335cc40afb738f60e63e5029
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-07-04 14:50:25 -0300

    Add logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,13 +1,32 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
+from user_app import models
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 @api_view(['POST', ])
 def registration_view(request):
 
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data)
 
+@api_view(['POST',])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
